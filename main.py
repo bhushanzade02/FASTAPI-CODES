59a00aeff896aa34bb7ddfbaa4b326c19a3ded3e
bhushanzade02 <bhushanzade02@gmail.com>
2025-06-01 17:17:34 +0530

    Update

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-from fastapi import FastAPI 
-app =FastAPI()
+# for virtual and creationa and activation commands
+# python -m venv myenv
+# myenv\Scripts\Activate
+# uvicorn main:app --reload
+from fastapi import FastAPI
+
+app = FastAPI()
 
 
 @app.get("/")
@@ -7,13 +12,6 @@ def hello():
     return {'message':'Hello World'}
 
 
-@app.get('/about')
+@app.get("/about")
 def about():
-    return {'mesage':'Machine learning is Good to Learn'}
-
-
-
-# how to enable virtual environment 
-# python -m venv myenv
-# myenv\Scripts\activate
-# uvicorn main::app --reload
+    return {'message':'Bhushan 1st Api'}
